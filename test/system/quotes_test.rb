b7970f5ef8b404412aad7adae47a8c4de9a45b0d
Jessie Rohrer <35381492+jrrohrer@users.noreply.github.com>
2022-07-12 23:43:42 -0400

    Chapter 4

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -2,45 +2,42 @@ require "application_system_test_case"
 
 class QuotesTest < ApplicationSystemTestCase
   setup do
-    @quote = quotes(:first) 
-  end
-  test "Creating a new quote" do
-    # When we visit the Quotes#index page
-    # we expect to see a title with the text "Quotes"
-    visit quotes_path
-    assert_selector "h1", text: "Quotes"
-
-    # When we click on the link with the text "New Quote"
-    # we expect to land on a page with the title "New Quote"
-    click_on "New Quote"
-    assert_selector "h1", text: "New Quote"
-
-    # When we fill in the name input with "Capybara Quote"
-    # and we click on "Create Quote"
-    fill_in "Name", with: "Capybara quote"
-    click_on "Create Quote"
-
-    # We expect to be back on the page with the title "Quotes"
-    # and to see our "Capybara quote" added to the list
-    assert_selector "h1", text: "Quotes"
-    assert_text "Capybara quote"
+    @quote = quotes(:first)
   end
 
   test "Showing a quote" do
     visit quotes_path
     click_link @quote.name
+
     assert_selector "h1", text: @quote.name
+  end
+
+  test "Creating a new quote" do
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    click_on "New quote"
+    fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
   end
 
   test "Updating a quote" do
     visit quotes_path
     assert_selector "h1", text: "Quotes"
+
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit Quote"
-    fill_in "Name", with: "Updated Quote"
-    click_on "Update Quote"
+    fill_in "Name", with: "Updated quote"
+
     assert_selector "h1", text: "Quotes"
-    assert_text "Updated Quote"
+    click_on "Update quote"
+
+    assert_selector "h1", text: "Quotes"
+    assert_text "Updated quote"
   end
 
   test "Destroying a quote" do
